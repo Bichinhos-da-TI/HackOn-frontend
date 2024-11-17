239d777fca21056061c5c4153c43671834ac2f22
Diego Oliveira <diego@thdr.com.br>
2024-11-17 12:46:14 -0300

    Cores adicionadas conforme Figma no arquivo tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-blue': '#2B3559',
+        'blue-duck': '#33C4CB',
+        'orange-frog': '#F76B00',
+        'yellow-cat': '#F7BC03',
+        'gray-ai':'#D2E3F3',
+        'gray': '#F5F9FC',
+        'white': '#FFFFFF'
+      },
+      fontFamily: {
+        'jura': ['Jura', 'sans-serif']
+      }
+
+    },
   },
   plugins: [],
 };
