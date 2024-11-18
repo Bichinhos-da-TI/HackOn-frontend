a4705e70a5c480c796d5b1daaf02c9addf0e5fa7
Matheus Dorigon <dorigon.matheus8@gmail.com>
2024-11-18 15:19:48 -0300

    consegui colocar a images do jeito que ta no figma mas tem que ajustar breakpoints com @layer

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,20 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'dark-blue': '#2B3559',
-        'blue-duck': '#33C4CB',
-        'orange-frog': '#F76B00',
-        'yellow-cat': '#F7BC03',
-        'gray-ai':'#D2E3F3',
-        'gray': '#F5F9FC',
-        'white': '#FFFFFF'
+        "dark-blue": "#2B3559",
+        "blue-duck": "#33C4CB",
+        "orange-frog": "#F76B00",
+        "yellow-cat": "#F7BC03",
+        "gray-ai": "#D2E3F3",
+        gray: "#F5F9FC",
+        white: "#FFFFFF",
       },
       fontFamily: {
-        'jura': ['Jura', 'sans-serif']
-      }
-
+        jura: ["Jura", "sans-serif"],
+      },
+      screens: {
+        "xl":"1280px"
+      },
     },
   },
   plugins: [],
