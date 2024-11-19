3d2a2cc04f1b7a42fbd3e09c7c5e3a60317ae6ac
Diego Oliveira <diego@thdr.com.br>
2024-11-19 15:12:22 -0300

    Alteracoes socilitados no pull request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { SubmitComponent } from './submit/submit.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SubmitComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -5,7 +5,6 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './submit.component.html',
-  styleUrl: './submit.component.css'
 })
 export class SubmitComponent {
 
