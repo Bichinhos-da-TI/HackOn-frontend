a0d406e539b725dfbdc2cf067370658bc9839634
= <=>
2024-11-25 14:38:22 -0300

    criando estrutura para o componente e label

---
+++ src/app/shared/input-wlabel/input-wlabel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-wlabel',
+  standalone: true,
+  imports: [],
+  templateUrl: './input-wlabel.component.html',
+  styleUrl: './input-wlabel.component.css'
+})
+export class InputWLabelComponent {
+
+}
