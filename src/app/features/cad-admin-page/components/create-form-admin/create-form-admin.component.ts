e24dc59cd32ded72dacc5a1f771cfeeabd362eb9
BrMD <dorigon.matheus8@gmail.com>
2024-11-26 14:59:11 -0300

    adicionei alguns styles padrao e possibilidades de adicionar custom styles no input

--- src/app/features/cad-admin-page/components/create-form-admin/create-form-admin.component.ts
+++ src/app/features/cad-admin-page/components/create-form-admin/create-form-admin.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { InputWLabelComponent } from '../../../../shared/input-wlabel/input-wlabel.component';
 
 @Component({
   selector: 'app-create-form-admin',
   standalone: true,
-  imports: [],
+  imports: [InputWLabelComponent],
   templateUrl: './create-form-admin.component.html',
-  styleUrl: './create-form-admin.component.css'
+  styleUrl: './create-form-admin.component.css',
 })
-export class CreateFormAdminComponent {
-
-}
+export class CreateFormAdminComponent {}
--- src/app/shared/input-wlabel/input-wlabel.component.ts
+++ src/app/shared/input-wlabel/input-wlabel.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-input-wlabel',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './input-wlabel.component.html',
-  styleUrl: './input-wlabel.component.css'
+  styleUrl: './input-wlabel.component.css',
 })
-export class InputWLabelComponent {
+export class InputWLabelComponent implements OnInit {
+  InputStyles: Record<string, string> = {};
+  @Input() name = '';
+  @Input() type = '';
+  @Input() borderColor: null | string = null;
+  @Input() borderWidth: null | string = null;
+  @Input() borderStyle: null | string = null;
+  @Input() borderRadius: null | string = null;
+  @Input() backgroundInput: null | string = null;
+  @Input() fontColor: null | string = null;
 
+  setInputStyles() {
+    this.InputStyles = {
+      'border-color': this.borderColor ? this.borderColor : '#333134',
+      'border-width': this.borderWidth ? this.borderWidth : '1px',
+      'border-style': this.borderStyle ? this.borderStyle : 'solid',
+      'border-radius': this.borderRadius ? this.borderRadius : '4px',
+      'background-color': this.backgroundInput
+        ? this.backgroundInput
+        : '#3e4a78',
+      'color': this.fontColor ? this.fontColor : '#FFFFFF',
+    };
+  }
+
+  ngOnInit(): void {
+    this.setInputStyles();
+  }
 }
